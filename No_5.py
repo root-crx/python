7cf1b916a42d012a7a7c1d10e0576884505f9b09
root-crx <128946386+root-crx@users.noreply.github.com>
2023-03-26 16:28:58 +0800

    Create No_5.py

---
+++ No_5.py
@@ -0,0 +1,11 @@
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+
+print(f"Down payment: ${down_payment}")
+#这是一个判断房子买家是否良心的代码
+# 这个也一样很无聊
