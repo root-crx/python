e0fe98f3646a588a76eeb93b640b678d74be76bb
root-crx <128946386+root-crx@users.noreply.github.com>
2023-03-26 16:27:11 +0800

    Create No_4.py

---
+++ No_4.py
@@ -0,0 +1,15 @@
+is_hot = False
+is_clod = False
+
+if is_hot:
+    print("It's a hot day.")
+    print('Drink plenty of water.')
+elif is_clod:
+    print("It's a clod day,")
+    print('Ware warm clothes.')
+else:
+    print("It's a lovely day.")
+
+print('Enjoy your day.')
+# 判断天气好坏的问候语
+# 这个比较无聊，直接跳过
